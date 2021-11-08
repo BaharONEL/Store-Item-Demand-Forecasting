14ebb27f1e1b5bd6451237ec0fee2aa81d6bbfbd
Bahar Önel <90307489+BaharONEL@users.noreply.github.com>
2021-11-08 21:38:43 +0300

    Add files via upload

---
+++ Store Item Demand Forecasting.py
@@ -0,0 +1,199 @@
+##################### Libraries ###########################
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import lightgbm as lgb
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+from sklearn.metrics import mean_absolute_error
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.api as sm
+import itertools
+
+import warnings
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+warnings.filterwarnings('ignore')
+
+################################### Load Dataset ####################################################
+
+train = pd.read_csv(r'C:\Users\Oguz\Desktop\DCMLBC06\HAFTA09\dataset\train.csv', parse_dates=['date'])
+test = pd.read_csv(r'C:\Users\Oguz\Desktop\DCMLBC06\HAFTA09\dataset\test.csv', parse_dates=['date'])
+df = pd.concat([train, test], sort=False)
+df.head()
+
+############################################### Check Data ############################################################
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+check_df(df)
+
+
+
+######################################## Date Features ################################################
+
+def create_date_features(df):
+    df['month'] = df.date.dt.month
+    df['day_of_month'] = df.date.dt.day
+    df['day_of_year'] = df.date.dt.dayofyear
+    df['week_of_year'] = df.date.dt.weekofyear
+    df['day_of_week'] = df.date.dt.dayofweek
+    df['year'] = df.date.dt.year
+    df["is_wknd"] = df.date.dt.weekday // 4
+    df['is_month_start'] = df.date.dt.is_month_start.astype(int)
+    df['is_month_end'] = df.date.dt.is_month_end.astype(int)
+    return df
+
+
+df = create_date_features(df)
+
+######################################## Random Noise ############################################
+def random_noise(dataframe):
+    return np.random.normal(scale=1.6, size=(len(dataframe),))
+
+################################## Lag/Shifted Features #################################################
+def lag_features(dataframe, lags):
+    for lag in lags:
+        dataframe['sales_lag_' + str(lag)] = dataframe.groupby(["store", "item"])['sales'].transform(
+            lambda x: x.shift(lag)) + random_noise(dataframe)
+    return dataframe
+
+df = lag_features(df, [91, 98, 105, 112, 119, 126, 182, 364, 546, 728])
+
+###################################### Rolling Mean Features ########################################
+
+def roll_mean_features(dataframe, windows):
+    for window in windows:
+        dataframe['sales_roll_mean_' + str(window)] = dataframe.groupby(["store", "item"])['sales']. \
+                                                          transform(
+            lambda x: x.shift(1).rolling(window=window, min_periods=10, win_type="triang").mean()) + random_noise(
+            dataframe)
+    return dataframe
+
+
+df = roll_mean_features(df, [365, 546, 730])
+
+
+################################# Exponentially Weighted Mean Features  #############################################
+def ewm_features(dataframe, alphas, lags):
+    for alpha in alphas:
+        for lag in lags:
+            dataframe['sales_ewm_alpha_' + str(alpha).replace(".", "") + "_lag_" + str(lag)] = \
+                dataframe.groupby(["store", "item"])['sales'].transform(lambda x: x.shift(lag).ewm(alpha=alpha).mean())
+    return dataframe
+
+
+alphas = [0.99, 0.95, 0.9, 0.8, 0.7, 0.5]
+lags = [91, 98, 105, 112, 180, 270, 365, 546, 728]
+
+df = ewm_features(df, alphas, lags)
+
+df = pd.get_dummies(df, columns=['day_of_week', 'month'])
+
+
+df['sales'] = np.log1p(df["sales"].values) #Converting sales to log(1+sales)
+
+############################################# Time-Based Validation Sets #############################################
+train = df.loc[(df["date"] < "2017-01-01"), :]
+
+val = df.loc[(df["date"] >= "2017-01-01") & (df["date"] < "2017-04-01"), :]
+
+cols = [col for col in train.columns if col not in ['date', 'id', "sales", "year"]]
+
+Y_train = train['sales']
+X_train = train[cols]
+
+Y_val = val['sales']
+X_val = val[cols]
+
+Y_train.shape, X_train.shape, Y_val.shape, X_val.shape
+
+#############################################  Custom Cost Function ############################################
+
+def smape(preds, target):
+    n = len(preds)
+    masked_arr = ~((preds == 0) & (target == 0))
+    preds, target = preds[masked_arr], target[masked_arr]
+    num = np.abs(preds - target)
+    denom = np.abs(preds) + np.abs(target)
+    smape_val = (200 * np.sum(num / denom)) / n
+    return smape_val
+
+def lgbm_smape(preds, train_data):
+    labels = train_data.get_label()
+    smape_val = smape(np.expm1(preds), np.expm1(labels))
+    return 'SMAPE', smape_val, False
+
+############################# Light GBM ####################################################################
+
+lgb_params = {'metric': {'mae'},
+              'num_leaves': 10,
+              'learning_rate': 0.02,
+              'feature_fraction': 0.8,
+              'max_depth': 5,
+              'verbose': 0,
+              'num_boost_round': 10000,
+              'early_stopping_rounds': 200,
+              'nthread': -1}
+lgbtrain = lgb.Dataset(data=X_train, label=Y_train, feature_name=cols)
+lgbval = lgb.Dataset(data=X_val, label=Y_val, reference=lgbtrain, feature_name=cols)
+
+model = lgb.train(lgb_params, lgbtrain,
+                  valid_sets=[lgbtrain, lgbval],
+                  num_boost_round=lgb_params['num_boost_round'],
+                  early_stopping_rounds=lgb_params['early_stopping_rounds'],
+                  feval=lgbm_smape, # hatyı gözlemliyoruz
+                  verbose_eval=100)
+
+y_pred_val = model.predict(X_val, num_iteration=model.best_iteration)
+
+
+smape(np.expm1(y_pred_val), np.expm1(Y_val))
+
+##################################### Final Model ##########################################################
+
+train = df.loc[~df.sales.isna()]
+Y_train = train['sales']
+X_train = train[cols]
+
+test = df.loc[df.sales.isna()]
+X_test = test[cols]
+
+lgb_params = {'metric': {'mae'},
+              'num_leaves': 10,
+              'learning_rate': 0.02,
+              'feature_fraction': 0.8,
+              'max_depth': 5,
+              'verbose': 0,
+              'nthread': -1,
+              "num_boost_round": model.best_iteration}
+
+lgbtrain_all = lgb.Dataset(data=X_train, label=Y_train, feature_name=cols)
+
+model = lgb.train(lgb_params, lgbtrain_all, num_boost_round=model.best_iteration)
+test_preds = model.predict(X_test, num_iteration=model.best_iteration)
+
+
+forecast = pd.DataFrame({"date":test["date"],
+                        "store":test["store"],
+                        "item":test["item"],
+                        "sales":test_preds
+                        })
+
+forecast[(forecast.store == 1) & (forecast.item == 1)].set_index("date").sales.plot(color = "green",
+                                                                                    figsize = (20,9),
+                                                                                    legend=True, label = "Store 1 Item 1 Forecast");
